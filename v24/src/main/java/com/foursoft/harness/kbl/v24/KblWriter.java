32480385d7097c1b5055b77cf5e81a545a49a4f9
ben <benedikt.linke@me.com>
2022-02-19 09:23:34 +0100

    updated license

--- v24/src/main/java/com/foursoft/harness/kbl/v24/KblWriter.java
+++ v24/src/main/java/com/foursoft/harness/kbl/v24/KblWriter.java
@@ -1,6 +1,6 @@
 /*-
  * ========================LICENSE_START=================================
- * kbl24
+ * kbl-v24
  * %%
  * Copyright (C) 2019 - 2020 4Soft GmbH
  * %%
@@ -10,10 +10,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * 
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
